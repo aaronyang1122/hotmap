966b7edacf00298670148cc8d755a93215c16f58
babyang <aaronyang1122@gmail.com>
2017-02-04 17:27:20 +0800

    1

--- server/router.js
+++ server/router.js
@@ -1,113 +1,136 @@
 'use strict';
 
-const URL = require('url');
 const FS = require('fs');
-const PATH = require('path');
-const types = {
-  "css": "text/css",
-  "gif": "image/gif",
-  "html": "text/html",
-  "ico": "image/x-icon",
-  "jpeg": "image/jpeg",
-  "jpg": "image/jpeg",
-  "js": "text/javascript",
-  "json": "application/json",
-  "pdf": "application/pdf",
-  "png": "image/png",
-  "svg": "image/svg+xml",
-  "swf": "application/x-shockwave-flash",
-  "tiff": "image/tiff",
-  "txt": "text/plain",
-  "wav": "audio/x-wav",
-  "wma": "audio/x-ms-wma",
-  "wmv": "video/x-ms-wmv",
-  "xml": "text/xml"
-};
+const express = require('express');
+const UA = require('ua-device');
+const phantom = require('phantom');
+const router = express.Router();
 
-var _data = []
+var _data = [];
 
-const toQueryParams = function () { 
-	var searchHash = this.split('&'); 
-	var ret = {};
-	for(var i = 0, len = searchHash.length; i < len; i++){ //这里可以调用each方法 
-		var pair = searchHash[i]; 
-		if((pair = pair.split('='))[0]){ 
-			var key = decodeURIComponent(pair.shift()); 
-			var value = pair.length > 1 ? pair.join('=') : pair[0]; 
-			if(value != undefined){ 
-				value = parseInt(decodeURIComponent(value)); 
-			} 
-			if(key in ret){ 
-				if(ret[key].constructor != Array){ 
-					ret[key] = [ret[key]]; 
-				} 
-				ret[key].push(value); 
-			}else{ 
-				ret[key] = value; 
-			} 
-		} 
-	} 
-	return ret; 
-}
+router.get('/pic', function (req, res) {
+	let _currentDate = new Date();
+	FS.readFile('./data/data.json', 'utf8', function (err, data) {
+	  if (err){
+	    console.log(err);
+	  } else {
+	  		_data = (data !== '') ? JSON.parse(data) : _data;
+	  		if (req.get('Referrer') !== undefined) {
+	  			let _o = Object.assign({
+	  				referer: req.get('Referrer'),
+	  				useragent: req.get('user-agent'),
+	  				device: new UA(req.get('user-agent')),
+	  				host: req.hostname,
+	  				originalurl: req.originalUrl,
+	  				path: req.path,
+	  				baseurl: req.baseUrl,
+	  				time: _currentDate.getTime()
+	  			}, req.query)
+	  			_data.push(_o)
+	  			FS.writeFile('./data/data.json', JSON.stringify(_data), 'utf8'); // write it back
+	  			// print
+	  			console.log('\n' + 
+											'====================================================================' + 
+											'\n' + 
+											'requestQuery: ' + JSON.stringify(req.query) +'\n' + 
+											'requestReferer: ' + req.get('Referrer') + '\n' + 
+											'requestUserAgent: ' + req.get('user-agent') + '\n' + 
+											'date: ' + _currentDate.getTime() + '\n' +
+											'====================================================================' + 
+											'\n')
+	  		}
+		}
+	});
+	res.type('png');
+	res.status(200).end();
+})
 
-const router = {
-	pic: function (req, res) {
-		let query = URL.parse(req.url).query;
-		let _currentDate = new Date();
-		res.writeHead(200, {'Content-Type': 'image/jpeg'});
-		FS.readFile('./assets/data.json', 'utf8', function readFileCallback(err, data){
-	    if (err){
-	      console.log(err);
-	    } else {
-	    		_data = (data !== '') ? JSON.parse(data) : _data;
-	    		if (req.headers.referer !== undefined) {
-	    			let _o = Object.assign({
-	    				referer: req.headers.referer,
-	    				useragent: req.headers['user-agent'],
-	    				time: _currentDate.getTime()
-	    			}, toQueryParams.call(query))
-	    			_data.push(_o)
-	    			FS.writeFile('./assets/data.json', JSON.stringify(_data), 'utf8'); // write it back 
-	    		}
-			}
-	  });
-		
-		console.log('\n' + 
-								'====================================================================' + 
-								'\n' + 
-								'requestQuery: ' + query +'\n' + 
-								'requestReferer: ' + req.headers.referer + '\n' + 
-								'requestUserAgent: ' + req.headers['user-agent'] + '\n' + 
-								'date: ' + _currentDate.getTime() + '\n' +
-								'====================================================================' + 
-								'\n')
-		res.end();
-	},
-	assets: function (req, res) {
-		let pathname = '.' + URL.parse(req.url).pathname;
-		let ext = PATH.parse(pathname).ext;
-		ext = ext ? ext.slice(1) : 'unknown';
-		let contentType = types[ext] || "text/plain";
-		
-		FS.exists(pathname, function (exists) {
-			if (!exists) {
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write("This request URL " + pathname + " was not found on this server.");
-        res.end();
-      } else {
-      	FS.readFile(pathname, "binary", function(err, file) {
-			    if (err) {
-		        res.writeHead(500, {'Content-Type': 'text/plain'});
-		        res.end(err);
-			    } else {
-		        res.writeHead(200, {'Content-Type': contentType});
-		        res.write(file, "binary");
-		        res.end();
-			    }
-			 	});
-      }
-		});
-	}
-}
+router.get('/hotmapdata', function (req, res) {
+	FS.readFile('./data/data.json', 'utf8', function (err, data) {
+	  if (err){
+	    console.log(err);
+	    res.status(500).send({ error: 'something error' });
+	  } else {
+  		res.send((data !== '') ? JSON.parse(data) : _data);
+		}
+	});
+})
+
+router.get('/screenshot', function (req, res) {
+  phantom.create().then(function (ph) {
+    ph.createPage().then(function (page) {
+      page.setting('userAgent', 'foo app');
+      page.open('http://phantomjs.org/screen-capture.html').then(function (status) {
+//      res.json({
+//          pageStatus: status
+//      });
+				page.render('./phantomjs.png');
+        page.close();
+        ph.exit();
+        res.status(200).end();
+      });
+    });
+  });
+})
+
+//const router = {
+//	pic: function (req, res) {
+//		let query = URL.parse(req.url).query;
+//		let _currentDate = new Date();
+//		res.writeHead(200, {'Content-Type': 'image/jpeg'});
+//		FS.readFile('./assets/data.json', 'utf8', function readFileCallback(err, data){
+//	    if (err){
+//	      console.log(err);
+//	    } else {
+//	    		_data = (data !== '') ? JSON.parse(data) : _data;
+//	    		if (req.headers.referer !== undefined) {
+//	    			let _o = Object.assign({
+//	    				referer: req.headers.referer,
+//	    				useragent: req.headers['user-agent'],
+//	    				time: _currentDate.getTime()
+//	    			}, toQueryParams.call(query))
+//	    			_data.push(_o)
+//	    			FS.writeFile('./assets/data.json', JSON.stringify(_data), 'utf8'); // write it back 
+//	    		}
+//			}
+//	  });
+//		
+//		console.log('\n' + 
+//								'====================================================================' + 
+//								'\n' + 
+//								'requestQuery: ' + query +'\n' + 
+//								'requestReferer: ' + req.headers.referer + '\n' + 
+//								'requestUserAgent: ' + req.headers['user-agent'] + '\n' + 
+//								'date: ' + _currentDate.getTime() + '\n' +
+//								'====================================================================' + 
+//								'\n')
+//		res.end();
+//	},
+//	assets: function (req, res) {
+//		let pathname = '.' + URL.parse(req.url).pathname;
+//		let ext = PATH.parse(pathname).ext;
+//		ext = ext ? ext.slice(1) : 'unknown';
+//		let contentType = types[ext] || "text/plain";
+//		
+//		FS.exists(pathname, function (exists) {
+//			if (!exists) {
+//      res.writeHead(404, {'Content-Type': 'text/plain'});
+//      res.write("This request URL " + pathname + " was not found on this server.");
+//      res.end();
+//    } else {
+//    	FS.readFile(pathname, "binary", function(err, file) {
+//			    if (err) {
+//		        res.writeHead(500, {'Content-Type': 'text/plain'});
+//		        res.end(err);
+//			    } else {
+//		        res.writeHead(200, {'Content-Type': contentType});
+//		        res.write(file, "binary");
+//		        res.end();
+//			    }
+//			 	});
+//    }
+//		});
+//	}
+//}
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,27 +1,37 @@
 'use strict';
+
+//const HTTP = require('http');
+//const URL = require('url');
+//const ROUTER = require('./router.js');
 const PORT = 9000;
-const HTTP = require('http');
-const URL = require('url');
-const ROUTER = require('./router.js');
+const path = require('path');
+const express = require('express');
+const router = require('./router.js');
+const app = express();
 
-HTTP.createServer(function (request, response){
-	if (request.url !== '/favicon.ico') {
-		// 获取路径
-		let pathname = URL.parse(request.url).pathname;
-		pathname = pathname.match(/\w+/)[0];
-		// router
-		if (ROUTER.hasOwnProperty(pathname)) {
-			ROUTER[pathname](request, response);
-		} else {
-			// 不在路由表里返回404
-			response.writeHead(404, {'Content-Type': 'text/plain'});
-      response.write("This request URL " + pathname + " was not found on this server.");
-      response.end();			
-		}
-	} else {
-		response.end();
-	}
-	
-}).listen(PORT);
+app.use(express.static(path.join(__dirname, '../assets')));
+app.use('/', router);
 
-console.log('Server running at 127.0.0.1:' + PORT)
+app.listen(PORT);
+
+console.log('Server running at 127.0.0.1:' + PORT);
+
+//HTTP.createServer(function (request, response){
+//	if (request.url !== '/favicon.ico') {
+//		// 获取路径
+//		let pathname = URL.parse(request.url).pathname;
+//		pathname = pathname.match(/\w+/)[0];
+//		// router
+//		if (ROUTER.hasOwnProperty(pathname)) {
+//			ROUTER[pathname](request, response);
+//		} else {
+//			// 不在路由表里返回404
+//			response.writeHead(404, {'Content-Type': 'text/plain'});
+//    response.write("This request URL " + pathname + " was not found on this server.");
+//    response.end();			
+//		}
+//	} else {
+//		response.end();
+//	}
+//}).listen(PORT);
+
